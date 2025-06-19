89ab9255bf44477d6b6b0c4654a41ce5cb23d666
Debora Sanchez <debora.sanchez.ga@gmail.com>
2025-06-19 14:02:26 -0400

    Create SQLAlchemy model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,23 @@
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Boolean
+from app.database import Base
 
 
 class Task(BaseModel):
 	title: str
 	description: str | None = None
 	completed: bool = False
+
+class TaskDB(Base):
+	__tablename__ = "tasks"
+
+	id = Column(Integer, primary_key=True, index=True)
+	title = Column(String, index=True)
+	description = Column(String, nullable=True)
+	completed = Column(Boolean, default=False)
+
+class TaskResponse(Task):
+	id: int
+
+	class Config:
+		orm_mode = True
